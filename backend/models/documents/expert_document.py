78c8f359527084d001accfeb3d3405c277b23ddf
narenSb1837 <snarenbalu@gmail.com>
2024-07-19 23:26:18 +0530

    feat: add backfill file for expert views

---
+++ backend/models/documents/expert_document.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import List, Any
+from pydantic import HttpUrl
+from backend.models.documents import BaseDocument, BaseDocumentMeta
+
+class ExpertDocumentMeta(BaseDocumentMeta):
+    title: str
+    source: str
+    time: datetime
+    author: str
+    content: str
+    description: str
+    company: List[str]
+    type: str
+    last_updated_time: datetime
+    link: HttpUrl
+
+class ExpertDocument(BaseDocument):
+    document_meta: ExpertDocumentMeta
+
+    def __init__(self, /, **data: Any):
+        super().__init__(**data)
