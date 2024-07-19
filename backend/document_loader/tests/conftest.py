13bbde1c7008ac0f623637f527368dafd5f6b26a
vsaravind01 <vsaravind01@gmail.com>
2024-07-19 20:21:27 +0530

    Remove source argument from NseIndexDocumentLoader class

--- backend/document_loader/tests/conftest.py
+++ backend/document_loader/tests/conftest.py
@@ -5,7 +5,7 @@ from backend.document_loader.url_config import NseUrlConfig
 
 
 @pytest.fixture
-def nse_index_data() -> tuple[str, list[dict]]:
+def nse_index_data() -> list[dict]:
     config = NseUrlConfig.get_index_config("NIFTY 50")
     response = requests.get(**config)
-    return config["url"], response.json()["data"]
+    return response.json()["data"]
