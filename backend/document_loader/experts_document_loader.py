fec3b12f592a96b8bc6c3740ce77473f803b1703
narenSb1837 <snarenbalu@gmail.com>
2024-07-19 23:26:18 +0530

    feat: add backfill file for expert views

---
+++ backend/document_loader/experts_document_loader.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import json
+from os import PathLike
+from typing import Optional
+
+from backend.document_loader.base_document_loader import BaseDataLoader
+from backend.models.documents.expert_document import ExpertDocument
+from backend.models.documents.expert_document import ExpertDocumentMeta
+
+class ExpertDataLoader(BaseDataLoader):
+    """Expert DataLoader class. Used to parse web-scraped."""
+
+    def __init__(self, file_path: PathLike, tag_set: Optional[list]):
+        self.file_path = file_path
+
+        super().__init__(tag_set)
+
+        self._initialize()
+
+    def _initialize(self):
+        """Initialize by loading the dataset"""
+        with open(self.file_path, "r") as file:
+            ds = json.load(file)
+            self._load_dataset(ds)
+
+    def _load_dataset(self, dataset: list[dict]):
+        """load the documents from the json dataset"""
+
+        for doc in dataset:
+            metadata = ExpertDocumentMeta(
+                title=doc["title"],
+                source=doc["source"],
+                time=doc["time"],
+                author=doc["author"],
+                description=doc["description"],
+                company=doc["company"],
+                type=doc["type"],
+                last_updated_time=doc["last_updated_time"],
+                link=doc["link"]
+                
+            )
+            document = ExpertDocument(
+                page_content=doc["content"], document_meta=metadata
+            )
+            self.documents.append(document)
+
